e474d1d1bded81dabd7d26bb27c28044c333c1cf
zadnap <zadnap@users.noreply.github.com>
2024-10-06 06:51:28 +0700

    Change UI of the projectListModal
    It now does not have a view button for each
    project item. If the user wants to choose one of
    the items, they just need to click on the item
    itself.

--- src/components/projectListModal.js
+++ src/components/projectListModal.js
@@ -1,40 +1,32 @@
 import "../assets/styles/modal.css";
 import "../assets/styles/projectListModal.css";
 
-import { getCurrentProject, getProjects } from "../modules/appController";
+import { getProjects } from "../modules/appController";
 
 function ProjectListModal() {
   const projectList = getProjects();
-  const currentProject = getCurrentProject();
 
   const projectListModal = document.createElement("div");
-  const container = document.createElement("div");
-
   projectListModal.classList.add("modal");
 
-  container.classList.add("container");
-  container.innerHTML = `
-    <div class="head">
-        <h4>Your Project List</h4>
-        <button class="btn btn-default"><i class="fa-solid fa-xmark"></i></button>
-    </div>
-    <div class="body">
-        ${projectList
-          .map((project) => {
-            return `<div class="project">
-            <h4 class="title">${project.title}</h4>
-            ${
-              currentProject === project
-                ? ""
-                : `<button class="btn btn-default">View</button>`
-            }
-          </div>`;
-          })
-          .join("")}
+  projectListModal.innerHTML = `
+    <div class="container">
+      <div class="head">
+          <h4>Your Project List</h4>
+          <button class="btn btn-default"><i class="fa-solid fa-xmark"></i></button>
+      </div>
+      <div class="body">
+          ${projectList
+            .map((project) => {
+              return `
+                <div class="project">
+                  <h4 class="title">${project.title}</h4>
+                </div>`;
+            })
+            .join("")}
+      </div>
     </div>
   `;
-
-  projectListModal.appendChild(container);
 
   return projectListModal;
 }
