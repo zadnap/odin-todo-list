530912c468032e42355193a4e43e65f7e84a2015
zadnap <zadnap@users.noreply.github.com>
2024-10-16 07:28:59 +0700

    Add editTodoModal UI component
    This modal allows user to edit their todo item
    including adding check list item to the list and
    modifying todo's title and description.

---
+++ src/components/editTodoModal.js
@@ -0,0 +1,53 @@
+import "../assets/styles/input.css";
+import "../assets/styles/modal.css";
+import "../assets/styles/editTodoModal.css";
+
+function createEditTodoModal() {
+  const editTodoModalElement = document.createElement("div");
+
+  editTodoModalElement.classList.add("modal");
+  editTodoModalElement.innerHTML = `
+        <div class="container">
+            <div class="head">
+                <h4>Edit Todo</h4>
+                <button class="btn btn-default btn-square"><i class="fa-solid fa-xmark"></i></button>
+            </div>
+            <div class="body">
+                <p>
+                    <label for="edit-todo-title">Title</label>
+                    <input type="text" placeholder="Edit title" id="edit-todo-title">
+                </p>
+                <p>
+                    <label for="edit-todo-desc">Description</label>
+                    <input type="text" placeholder="Edit description" id="edit-todo-desc">
+                </p>
+                <p>
+                    <label class="checklist-title">
+                        Checklist
+                        <button class="btn btn-default btn-square add-item"><i class="fa-solid fa-plus"></i></button>
+                    </label>
+                    <ul class="checklist">
+                        <li class="checklist-item">
+                            This is check list item 1
+                            <button class="btn btn-default btn-square"><i class="fa-solid fa-xmark"></i></button>
+                        </li>
+                        <li class="checklist-item">
+                            This is check list item 1
+                            <button class="btn btn-default btn-square"><i class="fa-solid fa-xmark"></i></button>
+                        </li>
+                        <li class="checklist-item new">
+                            <input type="text" placeholder="New">
+                        </li>
+                    </ul>
+                </p>
+            </div>
+            <div class="foot">
+                <button class="btn btn-primary">Save Changes</button>
+            </div>
+        </div>
+    `;
+
+  return editTodoModalElement;
+}
+
+export default createEditTodoModal;
